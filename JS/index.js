a8833f0b58dcdbbced91a44bf8173fc06adc5501
Van Hau <hn4901298@gmail.com>
2024-12-09 13:56:27 +0700

    Resolve merge conflicts

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,40 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const queryParams = new URLSearchParams(window.location.search);
-  const ssid = queryParams.get('ssid');
-  const networkType = queryParams.get('networkType');
-  const password = queryParams.get('password');
-  
-  if (ssid) {
-    document.getElementById('ssid').textContent = ssid;
+  const fullName = queryParams.get('fullName');
+  const phoneNumber = queryParams.get('phoneNumber');
+  const email = queryParams.get('email');
+  const address = queryParams.get('address');
+  const profileImage = queryParams.get('profileImage');
+  const website = queryParams.get('website');
+  const job = queryParams.get('job');
+
+  if (fullName) {
+    document.getElementById('fullName').textContent = fullName;
   }
 
-  if (networkType) {
-    document.getElementById('networkType').textContent = networkType;
+  if (phoneNumber) {
+    document.getElementById('phoneNumber').textContent = phoneNumber;
   }
 
-  if (password) {
-    document.getElementById('password').textContent = password;
+  if (email) {
+    document.getElementById('email').textContent = email;
+  }
+
+  if (address) {
+    document.getElementById('address').textContent = address;
+  }
+
+  if (profileImage) {
+    const imageElement = document.getElementById('profileImage');
+    imageElement.src = profileImage;
+    imageElement.style.display = 'block';
+  }
+
+  if (website) {
+    document.getElementById('website').textContent = website;
+  }
+
+  if (job) {
+    document.getElementById('job').textContent = job;
   }
 });
